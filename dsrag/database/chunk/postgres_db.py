c709a9345c021f0d1612500ca13b6c5302af4fd0
Abyl Ikhsanov <abyl@wheel.me>
2024-09-23 12:24:37 +0200

    testing pooling

--- dsrag/database/chunk/postgres_db.py
+++ dsrag/database/chunk/postgres_db.py
@@ -1,11 +1,15 @@
 import os
 import time
 import psycopg2
+from psycopg2 import pool
 from typing import Any, Optional
 import logging
+import threading
 from dsrag.database.chunk.db import ChunkDB
 from dsrag.database.chunk.types import FormattedDocument
 
+connection_pool = None
+pool_lock = threading.Lock()
 
 class PostgresDB(ChunkDB):
     def __init__(self, kb_id: str, connection_string: str) -> None:
@@ -26,28 +30,52 @@ class PostgresDB(ChunkDB):
             {"name": "supp_id", "type": "TEXT"},
             {"name": "metadata", "type": "TEXT"},
         ]
+        self.init_connection_pool()
+        self.create_table_if_not_exists()
 
-        # Create a table for this kb_id if it doesn't exist
-        conn = psycopg2.connect(self.connection_string)
-        c = conn.cursor()
-        c.execute(
-            "SELECT table_name FROM information_schema.tables WHERE table_name='documents'"
-        )
-        if not c.fetchone():
-            # Create the table if it doesn't exist
-            query_statement = "CREATE TABLE documents ("
-            for column in self.columns:
-                query_statement += f"{column['name']} {column['type']}, "
-            query_statement = query_statement[:-2] + ")"
-            c.execute(query_statement)
-            conn.commit()
-        conn.close()
+    def init_connection_pool(self):
+        global connection_pool
+        with pool_lock:
+            if connection_pool is None:
+                connection_pool = psycopg2.pool.ThreadedConnectionPool(
+                    minconn=1,
+                    maxconn=100,
+                    dsn=self.connection_string
+                )
+        logging.warning(f"The connection bool object is: {connection_pool}")
+
+    def get_connection(self):
+        return connection_pool.getconn()
+
+    def put_connection(self, conn):
+        connection_pool.putconn(conn)
+
+    def create_table_if_not_exists(self):
+        conn = self.get_connection()
+        try:
+            c = conn.cursor()
+            c.execute(
+                "SELECT table_name FROM information_schema.tables WHERE table_name='documents'"
+            )
+            if not c.fetchone():
+                query_statement = "CREATE TABLE documents ("
+                for column in self.columns:
+                    query_statement += f"{column['name']} {column['type']}, "
+                query_statement = query_statement[:-2] + ")"
+                c.execute(query_statement)
+                conn.commit()
+                # Create index
+                c.execute("CREATE INDEX idx_documents_doc_id_chunk_index ON documents (doc_id, chunk_index);")
+                conn.commit()
+            c.close()
+        finally:
+            self.put_connection(conn)
 
     def add_document(self, doc_id: str, chunks: dict[int, dict[str, Any]], supp_id: str = "",
                      metadata: dict = {}) -> None:
         # Add the docs to the PostgreSQL table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         # Create a created on timestamp
         created_on = str(int(time.time()))
@@ -90,7 +118,7 @@ class PostgresDB(ChunkDB):
     def remove_document(self, doc_id: str) -> None:
         # Remove the docs from the PostgreSQL table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute("DELETE FROM documents WHERE doc_id=%s", (doc_id,))
         conn.commit()
@@ -101,7 +129,7 @@ class PostgresDB(ChunkDB):
     ) -> Optional[FormattedDocument]:
         # Retrieve the document from the PostgreSQL table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         columns = ["supp_id", "document_title", "document_summary", "created_on", "metadata"]
         if include_content:
@@ -152,7 +180,7 @@ class PostgresDB(ChunkDB):
         doc_id = str(doc_id)
 
         # Retrieve the chunk text from the PostgreSQL table for the given range of chunk indices
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             "SELECT chunk_text FROM documents WHERE doc_id=%s AND chunk_index BETWEEN %s AND %s",
@@ -170,7 +198,7 @@ class PostgresDB(ChunkDB):
     def get_chunk_text(self, doc_id: str, chunk_index: int) -> Optional[str]:
         # Retrieve the chunk text from the PostgreSQL table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             "SELECT chunk_text FROM documents WHERE doc_id=%s AND chunk_index=%s", (doc_id, chunk_index)
@@ -185,7 +213,7 @@ class PostgresDB(ChunkDB):
     def get_chunk_page_numbers(self, doc_id: str, chunk_index: int) -> Optional[tuple[int, int]]:
         # Retrieve the chunk page numbers from the sqlite table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             "SELECT chunk_page_start, chunk_page_end FROM documents WHERE doc_id=%s AND chunk_index=%s", (doc_id, chunk_index)
@@ -198,7 +226,7 @@ class PostgresDB(ChunkDB):
 
     def get_document_title(self, doc_id: str, chunk_index: int) -> Optional[str]:
         # Retrieve the document title from the sqlite table
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             f"SELECT document_title FROM documents WHERE doc_id=%s AND chunk_index=%s", (str(doc_id), chunk_index)
@@ -212,7 +240,7 @@ class PostgresDB(ChunkDB):
     def get_document_summary(self, doc_id: str, chunk_index: int) -> Optional[str]:
         # Retrieve the document summary from the sqlite table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             f"SELECT document_summary FROM documents WHERE doc_id=%s AND chunk_index=%s", (doc_id, chunk_index)
@@ -226,7 +254,7 @@ class PostgresDB(ChunkDB):
     def get_section_title(self, doc_id: str, chunk_index: int) -> Optional[str]:
         # Retrieve the section title from the sqlite table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             f"SELECT section_title FROM documents WHERE doc_id=%s AND chunk_index=%s", (doc_id, chunk_index)
@@ -240,7 +268,7 @@ class PostgresDB(ChunkDB):
     def get_section_summary(self, doc_id: str, chunk_index: int) -> Optional[str]:
         # Retrieve the section summary from the sqlite table
         doc_id = str(doc_id)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute(
             f"SELECT section_summary FROM documents WHERE doc_id=%s AND chunk_index=%s", (doc_id, chunk_index)
@@ -253,7 +281,7 @@ class PostgresDB(ChunkDB):
 ####
     def get_all_doc_ids(self, supp_id: Optional[str] = None) -> list[str]:
         # Retrieve all document IDs from the PostgreSQL table
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         query_statement = "SELECT DISTINCT doc_id FROM documents"
         if supp_id:
@@ -267,7 +295,7 @@ class PostgresDB(ChunkDB):
 
     def get_document_count(self) -> int:
         # Retrieve the number of documents in the PostgreSQL table
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute("SELECT COUNT(DISTINCT doc_id) FROM documents")
         result = c.fetchone()
@@ -278,7 +306,7 @@ class PostgresDB(ChunkDB):
 
     def get_total_num_characters(self) -> int:
         # Retrieve the total number of characters in the PostgreSQL table
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute("SELECT SUM(chunk_length) FROM documents")
         result = c.fetchone()
@@ -289,7 +317,7 @@ class PostgresDB(ChunkDB):
 
     def delete(self) -> None:
         # Delete all documents (truncate table)
-        conn = psycopg2.connect(self.connection_string)
+        conn = self.get_connection()
         c = conn.cursor()
         c.execute("TRUNCATE TABLE documents")
         conn.commit()
